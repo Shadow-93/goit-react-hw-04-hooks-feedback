2a86fef1f8a72e5d69dcf8b4c7390095e9d418da
Andrey Hryhoriev <andreygrigoryev@ukr.net>
2020-12-28 21:35:11 +0200

    Updates

---
+++ precache-manifest.3264a56b506045fe4e4e4ae270bf684c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e13384e0a684e772c9fb05067beace94",
+    "url": "/goit-react-hw-04-hooks-feedback/index.html"
+  },
+  {
+    "revision": "a0ba35a87143893bc3e7",
+    "url": "/goit-react-hw-04-hooks-feedback/static/css/main.a2f43862.chunk.css"
+  },
+  {
+    "revision": "05d19d6916e34a13ab4a",
+    "url": "/goit-react-hw-04-hooks-feedback/static/js/2.7bd824f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-04-hooks-feedback/static/js/2.7bd824f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0ba35a87143893bc3e7",
+    "url": "/goit-react-hw-04-hooks-feedback/static/js/main.b1ad4ca0.chunk.js"
+  },
+  {
+    "revision": "567fea8629aa48e09379",
+    "url": "/goit-react-hw-04-hooks-feedback/static/js/runtime-main.a620f54e.js"
+  }
+]);
